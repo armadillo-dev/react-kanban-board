086f8e82d1ad184e0d0640d82b70459bbe72a490
armadillo-dev <armadillo.dev167@gmail.com>
2021-03-30 22:22:29 +0900

    resolve #8: 현재 상태 저장(local storage)
    - redux-persist를 활용해 현재 상태 저장

--- stores/index.ts
+++ stores/index.ts
@@ -1,18 +1,45 @@
-import { configureStore, createImmutableStateInvariantMiddleware } from '@reduxjs/toolkit'
+import {
+  combineReducers,
+  configureStore,
+  createImmutableStateInvariantMiddleware,
+} from '@reduxjs/toolkit'
+import {
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux'
 import { themeSlice } from './theme'
 import { issuesSlice } from './issues'
 import { statuesSlice } from './statuses'
 
+const persistConfig = {
+  key: 'root',
+  version: 1,
+  storage
+}
+
+const rootReducer = combineReducers({
+  theme: themeSlice.reducer,
+  issues: issuesSlice.reducer,
+  statues: statuesSlice.reducer,
+})
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
 const immutableInvariantMiddleware = createImmutableStateInvariantMiddleware()
 
 export const store = configureStore({
-  reducer: {
-    theme: themeSlice.reducer,
-    issues: issuesSlice.reducer,
-    statues: statuesSlice.reducer,
-  },
-  middleware: [immutableInvariantMiddleware],
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+    }
+  }).concat(immutableInvariantMiddleware),
 })
 
 
